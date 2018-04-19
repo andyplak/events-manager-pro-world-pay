07fc016c1bada5ca882bd7bd639ed55d7247f8eb
Andy Place <info@andyplace.co.uk>
2018-04-19 12:12:18 +0100

    NOOP remove debug

--- add-ons/gateways/gateway.worldpay.js
+++ add-ons/gateways/gateway.worldpay.js
@@ -4,7 +4,6 @@ $(document).bind('em_booking_gateway_add_worldpay', function(event, response){
   if(response.result){
     var wpForm = $('<form action="'+response.worldpay_url+'" method="post" id="em-worldpay-redirect-form"></form>');
     $.each( response.worldpay_vars, function(index,value){
-console.log( index+': '+value );
       wpForm.append('<input type="hidden" name="'+index+'" value="'+value+'" />');
     });
     wpForm.append('<input id="em-worldpay-submit" type="submit" style="display:none" />');
